1066cbd7b0353b6426801cedb8a4d73b8c0a8cd3
Manikavya Reddy <118108684+manikavyareddy27@users.noreply.github.com>
2024-03-19 22:31:07 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,48 @@
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from itertools import product
+from sklearn import metrics
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+
+PATH = os.path.join('data')
+
+actions = np.array(os.listdir(PATH))
+sequences = 30
+frames = 10
+
+label_map = {label:num for num, label in enumerate(actions)}
+
+landmarks, labels = [], []
+
+for action, sequence in product(actions, range(sequences)):
+    temp = []
+    for frame in range(frames):
+        npy = np.load(os.path.join(PATH, action, str(sequence), str(frame) + '.npy'))
+        temp.append(npy)
+    landmarks.append(temp)
+    labels.append(label_map[action])
+
+X, Y = np.array(landmarks), to_categorical(labels).astype(int)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.10, random_state=34, stratify=Y)
+
+model = Sequential()
+model.add(LSTM(32, return_sequences=True, activation='relu', input_shape=(10,126)))
+model.add(LSTM(64, return_sequences=True, activation='relu'))
+model.add(LSTM(32, return_sequences=False, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(X_train, Y_train, epochs=100)
+
+model.save('my_model')
+
+predictions = np.argmax(model.predict(X_test), axis=1)
+test_labels = np.argmax(Y_test, axis=1)
+
+accuracy = metrics.accuracy_score(test_labels, predictions)
